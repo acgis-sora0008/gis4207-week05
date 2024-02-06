cf0b5b2ad4caab9ccc5645b99ed2f66b93420108
acgis-amat0007 <amat0007@algonquinlive.com>
2024-02-06 15:32:24 -0500

    AA

--- MousmiS_AntonioA/describe06.py
+++ MousmiS_AntonioA/describe06.py
@@ -21,11 +21,11 @@ def desc_feature_class(fc):
     desc = arcpy.da.Describe(fc)
     print(f'{"BaseName":13} {desc["baseName"]}\n{"CatalogPath":13} {desc["catalogPath"]}\n{"DataType":13} {desc["dataType"]}')
     fields = desc["fields"]
-    print (fields)
 
-    # for field in fields:
-    #     print(f'{field:6}{})
+    print(f'{"Field Name":15} {"Field Type":15} {"Field Length":9}')
 
+    for field in fields:
+        print(f'{field.name:15} {field.type:15} {field.length:9}')
 
 
 if __name__ == "__main__":
