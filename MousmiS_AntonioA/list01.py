13dd10c635267dba4aeb708c15be7f40615fa8d6
Mousmi Sorathiya <sora0008@algonquinlive.com>
2024-02-06 15:47:31 -0500

    MS

--- MousmiS_AntonioA/list01.py
+++ MousmiS_AntonioA/list01.py
@@ -1,7 +1,7 @@
 import arcpy
 import os
 script_dir = os.path.dirname(os.path.abspath(__file__))
-workspace = os.path.join(script_dir, "..", "..", "..", "..", "data", "Week05_Data", "SanFrancisco")
+workspace = os.path.join(script_dir, '..\..\..\..\data\SanFrancisco")
 arcpy.env.workspace = workspace
 feature_classes = arcpy.ListFeatureClasses()
 for fc in feature_classes:
